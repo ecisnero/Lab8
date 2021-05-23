a04ae8b0455cf2d8c6475154541159f27efbd15c
ecisnero <ecisnero@ucsd.edu>
2021-05-22 23:59:44 -0700

    part 2 - first 16 tests

--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -83,27 +83,58 @@ describe('Basic user flow for SPA ', () => {
   it('Test10: Clicking the back button, new URL should be /#entry1', async () => {
     // implement test10: Clicking on the back button should update the URL to contain ‘/#entry1’
     await page.goBack();
-    const headerText = await page.$eval('h1', (e) => e.innerHTML);
-    expect(headerText).toBe('Entry 1');
+    const pageURL = await page.url();
+    expect(pageURL).toBe('http://127.0.0.1:5500/#entry1');
   });
 
   // define and implement test11: Clicking the back button once should bring the user back to the home page
-
+  it('Test11: Clicking the back button, new URL should be default one', async () => {
+    await page.goBack();
+    const pageURL = await page.url();
+    expect(pageURL).toBe('http://127.0.0.1:5500/');
+  });
 
   // define and implement test12: When the user if on the homepage, the header title should be “Journal Entries”
-
+  it('Test12: On homepage - checking header title', async () => {
+    const pageURL = await page.url();
+    const headerText = await page.$eval('h1', (e) => e.innerHTML);
+    expect(headerText).toBe('Journal Entries');
+  });
 
   // define and implement test13: On the home page the <body> element should not have any class attribute 
-
+  it('Test13: On homepage - checking <body> element class attribute', async () => {
+    const classAttribute = await page.$eval('body', (e) => e.getAttribute('class'));
+    expect(classAttribute).toBe('');
+  });
 
   // define and implement test14: Verify the url is correct when clicking on the second entry
-
+  it('Test14: Clicking second <journal-entry>, new URL should contain /#entry2', async () => {
+    const entries = await page.$$('journal-entry');
+    const secondEntry = await entries[1];
+    await secondEntry.click();
+    const pageURL = await page.url();
+    expect(pageURL).toBe('http://127.0.0.1:5500/#entry2');
+  });
 
   // define and implement test15: Verify the title is current when clicking on the second entry
-
+  it('Test15: On second Entry page - checking page header title', async () => {
+    const headerText = await page.$eval('h1', (e) => e.innerHTML);
+    expect(headerText).toBe('Entry 2');
+  });
 
   // define and implement test16: Verify the entry page contents is correct when clicking on the second entry
-
+  it('Test16: On first Entry page - checking <entry-page> contents', async () => {
+    const entryContent = await page.$eval('entry-page', (e) => e.entry);
+    expect(entryContent).toEqual({ 
+      title: 'Run, Forrest! Run!',
+      date: '4/26/2021',
+      content: "Mama always said life was like a box of chocolates. You never know what you're gonna get.",
+      image: {
+        src: 'https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg',
+        alt: 'forrest running'
+      }
+    });
+  }, 10000);
 
   // create your own test 17
 
